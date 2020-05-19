1016aebd22c5862a547dea9397b9d09551e7c822
wnag <wanglunia@sina.com>
2020-05-19 15:14:34 +0800

    update

--- code/dynamic-programming/fabonacci.js
+++ code/dynamic-programming/fabonacci.js
@@ -1,3 +1,4 @@
+//这是求解斐波那契数列的一道题，具体的斐波那契数列很好理解，可以上网去查其规律。斐波那契的递归解法是很经典也很基础的动态规划解法。
 //解法一：递归法
 function fib1(n){
     if(n<0){
@@ -29,24 +30,24 @@ function fib2(n){
 
 //解法三：备忘录法-自底向上
 function fib3(n){
-    if(n<=1){
+    if(n<=2){
         return memo[n]
     }else{
-        for(let i=2;i<=n;i++){
+        for(let i=3;i<=n;i++){
             memo[i]=memo[i-1]+memo[i-2]
         }
     }
     return memo[n]
 }
 
-//解法四：进一步压缩接发三的空间
+//解法四：进一步压缩解法三的空间
 function fib4(n){
     let memo1=1
     let memo2=1
     if(n<=1){
         return memo[n]
     }else{
-        for(let i=2;i<n;i++){
+        for(let i=3;i<=n;i++){
             memo_i=memo1+memo2
             memo1=memo2
             memo2=memo_i
@@ -56,4 +57,4 @@ function fib4(n){
 }
 
 //测试
-console.log(fib4(6))
+console.log(fib2(6))
